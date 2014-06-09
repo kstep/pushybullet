6b54e2355e373dbd1d7c0147b1499082e92cc50b
Konstantin Stepanov <me@kstep.me>
2014-06-10 02:44:47 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python2
+
+from distutils.core import setup
+
+setup(name='PushBullet',
+      version='1.0',
+      description='PushBullet APIv2 python bindings',
+      author='Konstantin Stepanov',
+      author_email='me@kstep.me',
+      url='http://github.com/kstep/pushbullet/',
+      py_modules=['pushbullet'],
+      scripts=['pb'])
