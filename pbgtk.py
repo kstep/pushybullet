c25490b3618ce7f500ee134b5a02bf04bcccde68
Konstantin Stepanov <me@kstep.me>
2014-08-07 21:38:15 +0300

    WIP: pbgtk

--- pbgtk.py
+++ pbgtk.py
@@ -1,24 +1,89 @@
 #!/usr/bin/env python2
+# -*- encoding: utf-8 -*-
 
 import os
 import sys
-import gtk
-import pynotify
 import pushybullet
+import requests
+import lxml.etree
+from gi.repository import Notify, Gtk, GdkPixbuf
 from multiprocessing import Process as Thread
+
+
+KNOWN_APPS = {
+        'com.andrew.apollo': 'Apollo'
+        }
+
+def get_play_app_name(package_name):
+    if not package_name:
+        return None
+
+    try:
+        return KNOWN_APPS[package_name]
+
+    except KeyError:
+        try:
+            name = KNOWN_APPS[package_name] = lxml.etree.HTML(
+                        requests.get(
+                            'https://play.google.com/store/apps/details',
+                                params={'id': package_name}
+                        ).content
+                    ).find(
+                        'body/'
+                        'div[@id="wrapper"]/'
+                        'div[@id="body-content"]/'
+                        'div/'
+                        'div[@class="details-info"]/'
+                        'div[@class="info-container"]/'
+                        'div[@class="document-title"]/'
+                        'div'
+                    ).text
+            return name
+
+        except:
+            return None
+
+class PushWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self)
+
+        vbox = Gtk.VBox()
+
+        title_text = self.title_edit = Gtk.Entry(placeholder_text='Note Title')
+        vbox.add(title_text)
+
+        body_text = self.body_text = Gtk.TextView()
+        vbox.add(body_text)
+
+        button_box = Gtk.Box()
+
+        push_button = Gtk.Button('Push')
+        button_box.add(push_button)
+
+        cancel_button = Gtk.Button('Cancel')
+        button_box.add(cancel_button)
+
+        vbox.add(button_box)
+
+        self.add(vbox)
+
+def open_browser(sender):
+    #os.system('%s https://www.pushbullet.com/' % os.getenv('BROWSER') or '/use/bin/google-chrome')
+    win = PushWindow()
+    win.show_all()
 
 def main():
     icon_path = os.path.realpath(os.path.join(os.path.dirname(__file__), "pushbullet.png"))
 
-    icon = gtk.StatusIcon()
+    icon = Gtk.StatusIcon()
     icon.set_from_file(icon_path)
     icon.set_tooltip_text("PushBullet")
     icon.set_visible(True)
-    icon.connect('activate', lambda s: gtk.main_quit())
+    icon.connect('activate', open_browser)
 
-    pynotify.init("PushBullet")
+    Notify.init("PushBullet")
 
-    gtk_thread = Thread(target=gtk.main)
+    gtk_thread = Thread(target=Gtk.main)
 
     try:
         pb = pushybullet.PushBullet(pushybullet.get_apikey_from_config() or sys.argv[1])
@@ -35,19 +100,25 @@ def main():
     def pb_watch():
         for ev in pb.stream(use_server_time=True):
             for push in ev.pushes(skip_empty=True):
-                try:
-                    if 'icon' in push:
-                        icon = '/tmp/pb-icon-%s.png' % push.package_name
-                        with open(icon, 'w') as f:
-                            f.write(push.icon)
-                    else:
-                        icon = icon_path
+                if push.type in ('dismissal'):
+                    continue
 
-                    notify = pynotify.Notification(
-                            push.get('title') or "PushBullet",
-                            push.get('body') or push.get('url'),
-                            icon
-                    )
+                try:
+                    print(str(type(push)), push.json())
+
+                    title = push.get('title') or get_play_app_name(push.get('package_name')) or "PushBullet"
+                    body = push.get('body') or push.get('url') or '\n'.join('— %s' % i for i in push.get('items')) or push.get('file_name')
+
+                    if 'icon' in push:
+                        loader = GdkPixbuf.PixbufLoader.new_with_type('jpeg')
+                        loader.write(push.icon)
+                        loader.close()
+
+                        notify = Notify.Notification.new(title, body)
+                        notify.set_icon_from_pixbuf(loader.get_pixbuf())
+
+                    else:
+                        notify = Notify.Notification.new(title, body, icon_path)
 
                     notify.show()
 
