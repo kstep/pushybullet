827d1bee9f49523834ef36ab0c64135c287f31dd
Konstantin Stepanov <me@kstep.me>
2015-01-13 18:55:43 +0300

    version bump to 1.5.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='PushyBullet',
-      version='1.5.1',
+      version='1.5.2',
       description='PushBullet APIv2 python bindings',
       author='Konstantin Stepanov',
       author_email='me@kstep.me',
